89cfd80794901c6ae71166bebdfc9e03f095feba
Meikelele <michal.bak2002@interia.pl>
2025-06-05 23:33:54 +0200

    sprinboot structure, created backend and frontend directories

---
+++ backend/src/main/java/com/example/myweightpal/MyweightpalApplication.java
@@ -0,0 +1,13 @@
+package com.example.myweightpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyweightpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyweightpalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/myweightpal/MyweightpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myweightpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyweightpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

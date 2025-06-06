cca3b3e7876a0c5886c4e4ade2621409a4d2769d
Meikelele <michal.bak2002@interia.pl>
2025-06-06 11:37:07 +0200

    created repo for WeightEntry

---
+++ backend/src/main/java/com/example/myweightpal/repository/WeightEntryRepository.java
@@ -0,0 +1,13 @@
+package com.example.myweightpal.repository;
+
+import com.example.myweightpal.model.WeightEntry;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface WeightEntryRepository extends MongoRepository<WeightEntry, String> {
+    List<WeightEntry> findByUserIdOrderByDateAsc(int userId);
+    Optional<WeightEntry> getWeightEntryByUserIdAndDate(int userId, LocalDate date);
+}

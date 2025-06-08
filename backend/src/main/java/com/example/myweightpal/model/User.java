5cd05d6db113f156754edede74d6a10acce0897c
Meikelele <michal.bak2002@interia.pl>
2025-06-08 17:47:12 +0200

    optimize imports in User entity

--- backend/src/main/java/com/example/myweightpal/model/User.java
+++ backend/src/main/java/com/example/myweightpal/model/User.java
@@ -3,9 +3,6 @@ package com.example.myweightpal.model;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;

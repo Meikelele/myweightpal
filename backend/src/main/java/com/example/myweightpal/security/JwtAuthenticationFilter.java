9e0789397bc0e790e0b5a7919aae7406bd639778
Meikelele <michal.bak2002@interia.pl>
2025-06-18 08:07:40 +0200

    add dependency AuthenticacationEntryPoint

--- backend/src/main/java/com/example/myweightpal/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/myweightpal/security/JwtAuthenticationFilter.java
@@ -1,4 +1,34 @@
 package com.example.myweightpal.security;
 
-public class JwtAuthenticationFilter {
+import com.example.myweightpal.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+
+// Czym jest komponent w springu
+
+/**
+ *  If user is not authenticated throws 401 error
+ * */
+@Component
+public class JwtAuthenticationFilter implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+            {
+                "error": "Unauthorized",
+                "message": "Access token is missing or invalid",
+                "status": 401
+            }
+            """);
+    }
 }

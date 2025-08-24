711d7b6489c32c07ab51193314d34237f4c810a2
Meikelele <michal.bak2002@interia.pl>
2025-08-24 18:20:36 +0200

    feat(styling): added global styling, changed color scheme

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, signal } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CardModule, ReactiveFormsModule, ButtonModule, InputTextModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  loading = signal(false);
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.loading.set(true);
+
+    const { email, password } = this.form.value;
+    console.log('LOGIN PAYLOAD', { email, password });
+
+    setTimeout(() => this.loading.set(false), 400);
+  }
+  
+  // submit() {
+  //   if (this.form.invalid) {
+  //     this.form.markAllAsTouched();
+  //     return;
+  //   }
+  //   this.loading.set(true);
+
+  //   // Na tym etapie tylko demo:
+  //   const { email, password } = this.form.value;
+  //   console.log('LOGIN PAYLOAD', { email, password });
+
+  //   // symulacja końca akcji
+  //   setTimeout(() => this.loading.set(false), 400);
+  // }
+
+  get f() {
+    return this.form.controls;
+  }
+
+}

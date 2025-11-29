32aa5ada14b42455a3ad7a489d4176d3e4a0e619
Meikelele <michal.bak2002@interia.pl>
2025-11-29 22:54:41 +0100

    feat(routes): added routing for /dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+  { path: 'dashboard', loadComponent: () => import("./pages/dashboard/dashboard.component").then(m => m.DashboardComponent)},
   { path: '**', redirectTo: '' }
 ];
 
